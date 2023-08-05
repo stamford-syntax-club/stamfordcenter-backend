bac8ac61b1f8bcf1d5a403e8344b5911695da78c
chinathaip <cartoonabe@gmail.com>
2023-08-05 22:00:07 +0700

    send file not working ;3;

---
+++ src/s3/client.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { S3Client } from "@aws-sdk/client-s3";
+
+dotenv.config();
+
+const s3Client = new S3Client({
+    endpoint: process.env.S3_ENDPOINT,
+});
+
+export default s3Client;
