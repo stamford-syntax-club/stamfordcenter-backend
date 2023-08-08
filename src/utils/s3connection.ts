c87e05e8dd1a48054b1384efdc7bd2c799a4c477
chinathaip <cartoonabe@gmail.com>
2023-08-08 23:43:40 +0700

    revert region

--- src/utils/s3connection.ts
+++ src/utils/s3connection.ts
@@ -4,7 +4,6 @@ import { S3Client } from "@aws-sdk/client-s3";
 dotenv.config();
 const s3Client = new S3Client({
     endpoint: process.env.S3_ENDPOINT,
-    region: undefined,
 });
 
 export { s3Client };
