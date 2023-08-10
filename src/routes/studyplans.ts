c48a263e93c89c92bdf7ec51565b99bb07d86a31
chinathaip <cartoonabe@gmail.com>
2023-08-10 21:29:12 +0700

    get_file api

--- src/routes/studyplans.ts
+++ src/routes/studyplans.ts
@@ -1,9 +1,5 @@
-import dotenv from "dotenv";
 import express, { Request, Response, Router } from "express";
-import { GetObjectCommand, NoSuchKey } from "@aws-sdk/client-s3";
 import { getConnection } from "../utils/mongoconnection";
-import { Readable } from "stream";
-import { s3Client } from "../utils/s3connection";
 import { convertResultToStudyPlan } from "../utils/converter";
 
 const studyplanRouter: Router = express.Router();
@@ -22,47 +18,6 @@ studyplanRouter.get("/", async (req: Request, res: Response) => {
 
     const studyplans = results.map((r) => convertResultToStudyPlan(r));
     res.json(studyplans);
-});
-
-studyplanRouter.get("/:fileKey", async (req: Request, res: Response) => {
-    if (!req.params.fileKey) {
-        res.status(400).send("file key is required");
-        return;
-    }
-
-    const cmd = new GetObjectCommand({
-        Bucket: "study-plans",
-        Key: req.params.fileKey,
-    });
-
-    try {
-        const response = await s3Client.send(cmd);
-
-        if (!response) {
-            return res.status(404).send("file not found");
-        }
-
-        if (req.query.download) {
-            res.attachment(req.params.fileName);
-        }
-        res.setHeader("Content-Type", response.ContentType ?? "application/octet-stream");
-
-        if (response.Body instanceof Readable) {
-            response.Body.once("error", (err) => {
-                console.error("Error downloading s3 file");
-                console.error(err);
-            });
-
-            return response.Body.pipe(res);
-        }
-    } catch (err) {
-        if (err instanceof NoSuchKey) {
-            res.status(404).send("file not found");
-            return;
-        }
-        res.status(500).send("internal server error");
-        console.error("send file: ", err);
-    }
 });
 
 export default studyplanRouter;
