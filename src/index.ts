4bf9ac56e2b53776baac8b444846da6f9373eece
Lxkas <alohasunshineday@yahoo.com>
2023-09-19 18:58:02 +0700

    export app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import 'module-alias/register';
+import fileRouter from "@routes/files.route";
+import studyplanRouter from "@routes/studyplans.route";
+import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
-import studyplanRouter from "@routes/studyplans.route";
-import fileRouter from "@routes/files.route";
-import cors from "cors";
-
+import "module-alias/register";
 
 dotenv.config();
 const app: Express = express();
@@ -14,7 +13,7 @@ app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
 });
 
-app.use(cors())
+app.use(cors());
 
 app.use("/api/study_plans", studyplanRouter);
 app.use("/api/files", fileRouter);
@@ -22,3 +21,5 @@ app.use("/api/files", fileRouter);
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+export default app;
