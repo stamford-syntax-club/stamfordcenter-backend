1f934cab56162669f907323bce98a13e902ded10
naris <narislyoko@gmail.com>
2024-02-23 22:42:19 +0700

    test 7

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import fileRouter from "@routes/files.route";
-import studyplanRouter from "@routes/studyplans.route";
+import studyplanRouter from "@routes/studyplans.route"  ;
 import resourceRouter from "@routes/resources.route";
 import cacheEndpoint from "@middlewares/rediscache";
 
 const app = express();
 app.get("/", (req: Request, res: Response) => {
-	res.send("Express + TypeScript Server");
-});
+  res.send("Express + TypeScript Server");
+}
+);
 
-app.use(cors());
+app.use(cors())
 app.use(cacheEndpoint); // Cache GET requests
 
-app.use("/api/study_plans", studyplanRouter); // to be removed after frontend has migrated
-app.use("/api/files", fileRouter);
-app.use("/api/resources", resourceRouter);
+app.use("/api/study_plans", studyplanRouter, ); // to be removed after frontend has migrated
+app.use("/api/files", fileRouter)
+app.use("/api/resources", resourceRouter)
 
 export default app;
